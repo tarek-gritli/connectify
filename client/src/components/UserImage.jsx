814108e2beb04a22de584820491786f2921a92eb
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-05-05 00:33:56 +0100

    Fix image of UserImage component to maintain aspect ratio

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 
-const UserImage = ({ image, size = "40px" }) => {
+const UserImage = ({ image, size = "50px" }) => {
   return (
-    <Box width={size - 20} height={size}>
+    <Box
+      width={size}
+      height={size}
+      sx={{ overflow: "hidden", borderRadius: "50%" }}
+    >
       <img
-        style={{ objectFit: "cover", borderRadius: "50%" }}
-        width={size}
-        height={size}
+        style={{ objectFit: "contain", width: "100%", height: "100%" }}
         alt="user"
         src={`http://localhost:3000/assets/${image}`}
       />
