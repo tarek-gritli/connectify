fcf6c3de121c67f496da601ab0f4ed78ac3ada6d
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-05-05 00:14:12 +0100

    Add MUI ThemeProvider, CssBaseline and protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,35 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route
+              path="/"
+              element={isAuth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <Profile /> : <Navigate to="/login" />}
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
